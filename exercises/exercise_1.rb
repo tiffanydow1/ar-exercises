a847ffa065cc5f90f83dd6bedc74ef3b78286d3e
Tiffany <tiffanyjdow@gmail.com>
2018-07-17 20:53:41 +0000

    Completed exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,30 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create(
+  name: 'Burnaby',
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+  )
+burnaby.save
+
+richmond = Store.create(
+  name: 'Richmond',
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+  )
+richmond.save
+
+gastown = Store.create(
+  name: 'Gastown',
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+  )
+gastown.save
+
+puts Store.count
+puts Store.all.inspect
